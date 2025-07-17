b5983601006a9fba85b897edf38403bfad827eac
Julya <99680402+julya-santos@users.noreply.github.com>
2025-07-16 21:37:33 -0300

    Create tabuada.py
    Gerador de Tabuada
    Descrição: Solicite ao usuário um número e, em seguida, exiba a tabuada de multiplicação desse número (1 a 10).

---
+++ tabuada.py
@@ -0,0 +1,12 @@
+print ("TABUADA")
+
+tabuada = int(input("Tabuada de: "))
+numero = 0
+cont = 1
+
+while (cont < 12):
+    resultado = tabuada * numero
+    print (tabuada,"x",numero,"=",resultado)
+    numero +=1
+    cont +=1
+    
